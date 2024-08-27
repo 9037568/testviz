3edccd8a8b204beac9b89a2d1a1fdd52c6115017
Chris Carman <namraccr@gmail.com>
2024-08-27 01:55:14 +0000

    First revisions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#st.write(
+#  "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#)
+
+choice = st.selectbox("Chart Type:", ["", "line", "bar"])
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+if (choice == "bar"):
+  st.bar_chart(df)
+elif (choice == "line"):
+  st.line_chart(df)
